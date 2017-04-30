66d2e2a81509cc690852e878e325fab7fc1fcea3
liujunqiang <liujunqiang@xunlei.com>
2017-05-01 00:37:52 +0800

    change: api error for response message

--- lesson201/user-center/src/main/java/io/junq/examples/common/web/RestResponseEntityExceptionHandler.java
+++ lesson201/user-center/src/main/java/io/junq/examples/common/web/RestResponseEntityExceptionHandler.java
@@ -1,5 +1,6 @@
 package io.junq.examples.common.web;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,10 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import io.junq.examples.common.web.exception.ApiError;
+import io.junq.examples.common.web.exception.IJBadRequestException;
+import io.junq.examples.common.web.exception.IJConflictException;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
@@ -26,7 +31,7 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
 	protected final ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 
-		return handleExceptionInternal(ex, "This http message is not readable.", headers,
+		return handleExceptionInternal(ex, message(HttpStatus.BAD_REQUEST, ex), headers,
 				HttpStatus.BAD_REQUEST, request);
 	}
    
@@ -34,12 +39,24 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
 	protected final ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 
-		return handleExceptionInternal(ex, "Method or argument is not valid.", headers, HttpStatus.BAD_REQUEST, request);
+		return handleExceptionInternal(ex, message(HttpStatus.BAD_REQUEST, ex), headers, HttpStatus.BAD_REQUEST, request);
 	}
     
-    @ExceptionHandler(value = {DataIntegrityViolationException.class})
+    @ExceptionHandler(value = {DataIntegrityViolationException.class, IJBadRequestException.class, IJConflictException.class})
     public ResponseEntity<Object> handleBadRequest(final RuntimeException ex, final WebRequest request) {
-    	return handleExceptionInternal(ex, "This is a bad request", new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    	return handleExceptionInternal(ex, message(HttpStatus.BAD_REQUEST, ex), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
-
+    
+    // 500
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<Object> handleServerError(final RuntimeException ex, final WebRequest request) {
+    	return handleExceptionInternal(ex, message(HttpStatus.INTERNAL_SERVER_ERROR, ex), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+    
+    private final ApiError message(final HttpStatus httpStatus, final Exception ex) {
+    	final String message = ex.getMessage() == null ? ex.getClass().getSimpleName() : ex.getMessage();
+    	final String developerMessage = ExceptionUtils.getRootCauseMessage(ex);
+    	
+    	return new ApiError(httpStatus.value(), message, developerMessage);
+    }
 }
