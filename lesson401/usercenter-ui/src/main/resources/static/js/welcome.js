bc0157be08bde5de924f02d8bdf6aafc17008fd6
liujunqiang <liujunqiang@xunlei.com>
2017-10-09 15:23:46 +0800

    fix: angular version

--- lesson401/usercenter-ui/src/main/resources/static/js/welcome.js
+++ lesson401/usercenter-ui/src/main/resources/static/js/welcome.js
@@ -1,4 +1,4 @@
-angular.module('welcome ', [ 'ngRoute' ]).config(function($routeProvider) {
+angular.module('welcome', [ 'ngRoute' ]).config(function($routeProvider) {
 
     $routeProvider.when('/', {
         templateUrl : 'home.html',
